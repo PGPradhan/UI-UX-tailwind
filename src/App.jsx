18d96595b7de04f6174e077252edfefda162c721
PGPradhan <pradhan.prathamesh10@gmail.com>
2024-03-17 13:34:22 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import './App.css'
-
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient";
+const App = () =>  {
   
 
   return (
-    <h1 className="text-3xl font-bold underline">
+   <>
+  <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-  )
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <Button className="mt-10" href="#">
+        Something
+      </Button>
+    </div>
+   </>
+ 
+    
+  ) 
 }
 
 export default App
